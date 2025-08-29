0bff19b01f1f33576089aded01371426a23fe460
Mohammad Jahanseir <48261288+mjahanseir@users.noreply.github.com>
2025-08-28 23:34:51 -0600

    Delete PL-300 directory

--- PL-300/Allfiles/Setup/UpdateDatabase-9-Reset.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host -foreground green 'Resetting database to original state...';
-
-Invoke-SqlCmd -Server localhost -Database "AdventureWorksDW2020" -InputFile "Scripts\UpdateDatabase-9.sql"
-
-Write-Host 'Press any key to close...';
-$z = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
